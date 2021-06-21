98852fae7c2daf6832359f1cb9f4d125f03f928c
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-06-21 23:18:04 +0530

    animations

--- Page-2/script.js
+++ Page-2/script.js
@@ -1,12 +1,21 @@
 function showsignup()
 {
     document.getElementById('signup').style.animation = 'signupanime 0.5s 1';
+    document.getElementById('heading2').style.animation = 'headanime 0.5s 1';
+    document.getElementById('username2').style.animation = 'inputanime 0.7s 1';
+    document.getElementById('password2').style.animation = 'inputanime 0.7s 1';
+    document.getElementById('email').style.animation = 'inputanime 0.7s 1';
+    document.getElementById('btn2').style.animation = 'btnanime 0.7s 1';
     document.getElementById('login').style.display = 'none';
     document.getElementById('signup').style.display = 'block';
 }
 function showlogin()
 {
     document.getElementById('login').style.animation = 'loginanime 0.5s 1';
+    document.getElementById('heading1').style.animation = 'headanime 0.5s 1';
+    document.getElementById('username').style.animation = 'inputanime 0.7s 1';
+    document.getElementById('password').style.animation = 'inputanime 0.7s 1';
+    document.getElementById('btn').style.animation = 'btnanime 0.7s 1';
     document.getElementById('login').style.display = 'block';
     document.getElementById('signup').style.display = 'none';
 }
