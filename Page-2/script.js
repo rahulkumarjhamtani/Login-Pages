8c1308901d4f8d8cd31785f8233e224139c83992
Rahul Kumar Jhamtani <77382243+rahulkumarjhamtani@users.noreply.github.com>
2021-06-21 15:07:15 +0530

    new updations

--- Page-2/script.js
+++ Page-2/script.js
@@ -1,10 +1,21 @@
+function showsignup()
+{
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('signup').style.display = 'block';
+}
+function showlogin()
+{
+    document.getElementById('login').style.display = 'block';
+    document.getElementById('signup').style.display = 'none';
+}
+
 function validate() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
     if(username=="admin" && password=="admin")
     {
-        document.getElementById('myform').action = '../index.html';
+        document.getElementById('myloginform').action = '../index.html';
         alert("Login successfully");
     }
     else
@@ -12,4 +23,11 @@ function validate() {
         alert("Incorrect Username or Password");
         return false; 
     }
+}
+
+function newuser() {
+    
+    document.getElementById('mysignupform').action = '../index.html';
+    alert("Thanks for registration!!!");
+
 }
